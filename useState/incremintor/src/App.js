54d1a905eb87b5d60734334af17b3daf790949ae
Omar Bitar <o.bitar97@gmail.com>
2022-03-16 21:01:36 -0600

    implemented useState

--- useState/incremintor/src/App.js
+++ useState/incremintor/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count,setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
+        <p>{count}</p>
+        <span>
+          <button onClick={() => setCount(count-1)}>-</button>
+          <button onClick={() => setCount(count+1)}>+</button>
+        </span>
+       
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
